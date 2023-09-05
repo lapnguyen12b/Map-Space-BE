ce49348c08278d6479b17f31fb409056f64c3be6
lapnv <lapnguyen12b@gmail.com>
2023-09-05 16:21:17 +0700

    add decorator

---
+++ src/user/dto/user-update.dto.ts
@@ -0,0 +1,29 @@
+import { Optional } from '@nestjs/common'
+import { IsEmail, IsEnum, IsString } from 'class-validator'
+import { Role, STATUS } from 'src/enums/status.enum'
+
+
+export class UserUpdateDto {
+  @Optional()
+  @IsString()
+  readonly userName: string
+
+  @Optional()
+  @IsString()
+  readonly password: string
+
+  @Optional()
+  readonly isSendEmailWelcome: boolean
+
+  @Optional()
+  @IsEmail({}, { message: 'Email must be valid' })
+  readonly email: string
+
+  @Optional()
+  @IsString()
+  readonly status: STATUS
+
+  @Optional()
+  @IsEnum(Role)
+  readonly role: Role
+}
